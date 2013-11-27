023762c23cf0e35c701d54a84e2884ac7c08abc9
Suke211 <skj.pass.2011@gmail.com>
2013-11-27 16:36:09 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirsApp::Application.config.secret_key_base = '6ecce44b7f4fbbbe9a603653605bbc2e5290c81c7d01a8e823cc1dd21195bed359e297d9f8d528e8d60b18ce0f2a1b6a1bb947195d6ad51a8fc1629536e104b7'
